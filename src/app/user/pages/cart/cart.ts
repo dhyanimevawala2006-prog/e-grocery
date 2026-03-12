101e97621f92efde80a452f543b87cc1f4b65910
Dhruvil-Mavawala <dhruvilmavawala4@gmail.com>
2026-03-13 01:00:41 +0530

    Dhruvi-12th-FRONT

--- src/app/user/pages/cart/cart.ts
+++ src/app/user/pages/cart/cart.ts
@@ -2,91 +2,119 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { CartService } from '../../../service/cart-service';
 import { NgFor, NgIf } from '@angular/common';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cart',
-  standalone:true,
+  standalone: true,
   templateUrl: './cart.html',
   styleUrls: ['./cart.css'],
-  imports:[NgFor,NgIf]
+  imports: [NgFor, NgIf, FormsModule],
 })
 export class Cart {
-
-  cartItems:any[] = [];
-  total:number = 0;
+  cartItems: any[] = [];
+  total: number = 0;
 
   userId = sessionStorage.getItem('id');
 
-  imageUrl="http://localhost:3000/upload/";
+  imageUrl = 'http://localhost:3000/upload/';
+
+  couponCode: string = '';
+  discount: number = 0;
+  finalTotal: number = 0;
 
   constructor(
-    private cartService:CartService,
-    private cdr:ChangeDetectorRef,
-    private router:Router
-  ){}
+    private cartService: CartService,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private http: HttpClient,
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.loadCart();
   }
 
-  loadCart(){
-
-    this.cartService.getCart(this.userId).subscribe((res:any)=>{
-
-      if(res.data){
+  loadCart() {
+    this.cartService.getCart(this.userId).subscribe((res: any) => {
+      if (res.data) {
         this.cartItems = res.data.items;
       }
 
       this.calculateTotal();
       this.cdr.detectChanges();
-
     });
-
   }
 
-  increase(productId:any){
-    this.cartService.increaseQty(this.userId,productId).subscribe(()=>{
+  increase(productId: any) {
+    this.cartService.increaseQty(this.userId, productId).subscribe(() => {
       this.loadCart();
     });
   }
 
-  decrease(productId:any){
-    this.cartService.decreaseQty(this.userId,productId).subscribe(()=>{
+  decrease(productId: any) {
+    this.cartService.decreaseQty(this.userId, productId).subscribe(() => {
       this.loadCart();
     });
   }
 
-  remove(productId:any){
-    this.cartService.removeItem(this.userId,productId).subscribe(()=>{
+  remove(productId: any) {
+    this.cartService.removeItem(this.userId, productId).subscribe(() => {
       this.loadCart();
     });
   }
 
-  calculateTotal(){
-
+  calculateTotal() {
     this.total = 0;
 
-    this.cartItems.forEach((item:any)=>{
+    this.cartItems.forEach((item: any) => {
       this.total += item.productId.price * item.quantity;
     });
-
   }
 
-  // CHECKOUT FUNCTION
-  checkout(){
+  // APPLY COUPON
+  applyCoupon(code: string) {
+    const coupon = code.trim();
 
-    if(this.cartItems.length === 0){
-      alert("Your cart is empty");
+    // Debug line
+    console.log('Coupon:', coupon);
+
+    if (!coupon) {
+      alert('Enter Coupon Code');
       return;
     }
 
-    this.router.navigate(['/checkout'],{
-      state:{
-        items:this.cartItems,
-        total:this.total
-      }
-    });
+    const data = {
+      code: coupon,
+      userId: this.userId,
+      cartTotal: this.total,
+    };
 
+    this.http.post('http://localhost:3000/api/coupon/apply', data).subscribe(
+      (res: any) => {
+        this.discount = res.discount;
+        this.finalTotal = res.finalTotal;
+      },
+      () => {
+        alert('Invalid Coupon');
+      },
+    );
   }
 
+  // CHECKOUT FUNCTION
+  checkout() {
+    if (this.cartItems.length === 0) {
+      alert('Your cart is empty');
+      return;
+    }
+
+    this.router.navigate(['/checkout'], {
+      state: {
+        items: this.cartItems,
+        total: this.total,
+        discount: this.discount,
+        couponCode: this.couponCode,
+      },
+    });
+  }
 }
--- src/app/user/pages/checkout/checkout.ts
+++ src/app/user/pages/checkout/checkout.ts
@@ -1,116 +1,126 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
 import Swal from 'sweetalert2';
 import { CartService } from '../../../service/cart-service';
 import { OrderService } from '../../../service/order-service';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.html',
-  imports:[NgFor,FormsModule],
-  styleUrls: ['./checkout.css']
+  imports: [NgFor, FormsModule],
+  styleUrls: ['./checkout.css'],
 })
-
 export class Checkout {
+  cartItems: any[] = [];
+  total: number = 0;
 
-  cartItems:any[]=[];
-  total:number = 0;
+  // coupon variables
+  discount: number = 0;
+  couponCode: string = '';
 
-  name:string="";
-  phone:string="";
-  address:string="";
-  city:string="";
+  name: string = '';
+  phone: string = '';
+  address: string = '';
+  city: string = '';
 
-  userId=sessionStorage.getItem('id');
+  userId = sessionStorage.getItem('id');
 
-  imageUrl="http://localhost:3000/upload/";
+  imageUrl = 'http://localhost:3000/upload/';
 
   constructor(
-    private cartService:CartService,
-    private orderService:OrderService,
-    private cdr:ChangeDetectorRef
-  ){}
+    private cartService: CartService,
+    private orderService: OrderService,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.loadCart();
+
+    // GET COUPON DATA FROM CART PAGE
+    const state: any = history.state;
+
+    if (state) {
+      this.discount = state.discount || 0;
+      this.couponCode = state.couponCode || '';
+    }
   }
 
   // LOAD CART
-  loadCart(){
-    this.cartService.getCart(this.userId).subscribe((res:any)=>{
+  loadCart() {
+    this.cartService.getCart(this.userId).subscribe((res: any) => {
       this.cartItems = res.data.items;
-      // alert(this.cartItems);
+
       this.calculateTotal();
+
       this.cdr.detectChanges();
     });
   }
 
   // CALCULATE TOTAL
-  calculateTotal(){
+  calculateTotal() {
     this.total = 0;
 
-    this.cartItems.forEach((item:any)=>{
+    this.cartItems.forEach((item: any) => {
       this.total += item.productId.price * item.quantity;
     });
   }
 
   // PLACE ORDER
- placeOrder(){
-
-  if(!this.name || !this.phone || !this.address || !this.city){
-    Swal.fire({
-      icon:'warning',
-      title:'Please fill all fields'
-    });
-    return;
-  }
-
-  const items = this.cartItems.map((item:any)=>({
-    productId:item.productId._id,
-    quantity:item.quantity
-  }));
-
-  const orderData = {
-    userId:this.userId,
-    items:items,
-    total:this.total + 40,
-    address:{
-      name:this.name,
-      phone:this.phone,
-      address:this.address,
-      city:this.city
-    }
-  };
-
-  this.orderService.createOrder(orderData).subscribe({
-
-    next:(res:any)=>{
-
+  placeOrder() {
+    if (!this.name || !this.phone || !this.address || !this.city) {
       Swal.fire({
-        icon:'success',
-        title:'Order Placed!',
-        text:'Your food is on the way 🍔',
-        confirmButtonColor:'#ff6b00'
-      }).then(()=>{
-        
-        // CLEAR CART
-        this.cartItems=[];
-        this.total=0;
-
-        // REDIRECT TO HOME
-       window.location.href='/';
-
+        icon: 'warning',
+        title: 'Please fill all fields',
       });
 
-    },
-
-    error:(err)=>{
-      console.log(err);
+      return;
     }
 
-  });
+    const items = this.cartItems.map((item: any) => ({
+      productId: item.productId._id,
+      quantity: item.quantity,
+    }));
 
-}
+    const orderData = {
+      userId: this.userId,
 
+      items: items,
+
+      total: this.total + 40,
+
+      discount: this.discount,
+
+      couponCode: this.couponCode,
+
+      address: {
+        name: this.name,
+        phone: this.phone,
+        address: this.address,
+        city: this.city,
+      },
+    };
+
+    this.orderService.createOrder(orderData).subscribe({
+      next: (res: any) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Order Placed!',
+          text: 'Your food is on the way 🍔',
+          confirmButtonColor: '#ff6b00',
+        }).then(() => {
+          this.cartItems = [];
+          this.total = 0;
+
+          window.location.href = '/';
+        });
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
